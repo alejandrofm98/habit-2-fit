b9f3ad9ca60eccb338d2cf45584185948ba4d62d
Jose Luis Fernandez <fernandezmateojoseluis@gmail.com>
2025-05-06 17:31:33 +0200

    Added google login

--- App.js
+++ App.js
@@ -8,16 +8,45 @@ import {
   Alert
 } from 'react-native';
 
+import auth from '@react-native-firebase/auth';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import {WEBCLIENT_ID} from '@env';
+
 const App = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleLogin = () => {
-    if (email === 'user@test.com' && password === '1234') {
+  GoogleSignin.configure({
+    scopes: ['https://www.googleapis.com/auth/userinfo.email','https://www.googleapis.com/auth/userinfo.profile'],
+    webClientId: WEBCLIENT_ID, // Client ID of type WEB for Firebase authentication
+  });
+
+  const handleLogin = async () => {
+    /*if (email === 'user@test.com' && password === '1234') {
       Alert.alert('Login exitoso');
     } else {
       Alert.alert('Credenciales incorrectas');
-    }
+    }*/
+        // Check if your device supports Google Play
+        //await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
+        // Get the users ID token
+        const signInResult = await GoogleSignin.signIn();
+        console.log('signInResult', signInResult);
+        // Try the new style of google-sign in result, from v13+ of that module
+        idToken = signInResult.data?.idToken;
+        if (!idToken) {
+          // if you are using older versions of google-signin, try old style result
+          idToken = signInResult.idToken;
+        }
+        if (!idToken) {
+          throw new Error('No ID token found');
+        }
+      
+        // Create a Google credential with the token
+        const googleCredential = auth.GoogleAuthProvider.credential(signInResult.data.idToken);
+      
+        // Sign-in the user with the credential
+        return auth().signInWithCredential(googleCredential);
   };
 
   return (
@@ -38,7 +67,7 @@ const App = () => {
         onChangeText={setPassword}
         secureTextEntry
       />
-      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+      <TouchableOpacity style={styles.button} onPress={() => handleLogin().then(() => Alert.alert('Login exitoso')).catch((error) => Alert.alert('Error', error.message))}>
         <Text style={styles.buttonText}>Entrar</Text>
       </TouchableOpacity>
     </View>
